a91fbed21e8858a5e080ba6d41b4c1318b47290e
Takudzwa Katyora <tkatyora7@gmail.com>
2025-06-12 02:35:33 +0200

    latest

--- scripts/rename.py
+++ scripts/rename.py
@@ -1,6 +1,6 @@
 import os
 
-folder_path = "/home/takudzwa/Documents/Projects/broiler_disease/detection/FOMO/dataset/train/healthy"
+folder_path = "/home/takudzwa/Documents/Projects/broiler_disease/detection/FOMO/ESP_32/train/healthy"
 
 image_files = [f for f in os.listdir(folder_path) if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
 
@@ -9,7 +9,7 @@ image_files.sort()
 
 
 for i, filename in enumerate(image_files, start=1):
-    new_name = f"healthy_{i}.jpg"
+    new_name = f"health_{i}.jpg"
     src = os.path.join(folder_path, filename)
     dst = os.path.join(folder_path, new_name)
     os.rename(src, dst)
